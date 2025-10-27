386674f66121158ee171dcfdb38969b1e1743363
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-10-28 01:27:26 +0300

    Update PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,21 @@
 module.exports = {
-    apps: [
-        {
-            name: 'CMS Rozenkrants',
-            port: '4208',
-            exec_mode: 'cluster',
-            instances: 'max',
-            script: './dist/server.js',
-        }
-    ]
+  apps: [
+    {
+      name: 'cms-psychology',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 4208',
+      cwd: '/var/www/cms.psychology',
+      interpreter: 'node',
+      exec_mode: 'fork',
+      instances: 1,
+      env: {
+        NODE_ENV: 'production',
+        NODE_OPTIONS: '--no-deprecation',
+        PORT: 4208
+      },
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      time: true
+    }
+  ]
 }
